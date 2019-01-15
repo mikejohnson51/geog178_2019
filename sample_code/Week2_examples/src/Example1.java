2715f83c71c6dc270bab617e1c386ab21db17d6c
mikejohnson51 <mike.johnson@geog.ucsb.edu>
2019-01-14 18:46:05 -0800

    week2 section materials
    sample code
    slides for varibables, loops and debugging
    OGC simple features slides

---
+++ sample_code/Week2_examples/src/Example1.java
@@ -0,0 +1,22 @@
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		//Location of interest given as a string variable
+		String loc1 = "UCSB";
+		
+		//The latitude of location 1 is given as a double variable
+		double lat1 = 34.4139;
+		
+		//The longitude of location 1 is given as a double variable
+		double lon1 = -119.8489;
+		
+		// A print statement is used to combine our three variables
+		
+		System.out.print(loc1 + " is located at " + 
+						 lat1 + " degress latitude and " + 
+						 lon1 + " degrees longitude.");
+
+	}
+
+}
